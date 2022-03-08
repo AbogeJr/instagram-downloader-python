2d3e2c4b821dcfd1c1835c75bfc9370d67e2c3df
Aloys Aboge Jr <76847396+AbogeJr@users.noreply.github.com>
2022-03-08 20:27:54 +0300

    fixes

--- downloader.py
+++ downloader.py
@@ -3,6 +3,7 @@ from flask import Flask, render_template, url_for, flash, redirect
 import requests
 import re
 from forms import URLForm 
+import os
 
 
 app = Flask(__name__)
@@ -34,38 +35,6 @@ def register():
         pic_urls = prepare_urls(pic_matches)
         return render_template('results.html', form=form, vids=vid_urls, pics=pic_urls, title="Results")
     return render_template('index.html', form=form)     
-    
-
-# @app.route("/results")
-# def results():
-#     return render_template('results.html')
-
-    # response = get_response(url)
-    # vid_matches = re.findall('"video_url":"([^"]+)"',response)
-    # pic_matches = re.findall('"display_url":"([^"]+)"', response)
-
-    # vid_urls = prepare_urls(vid_matches)
-    # pic_urls = prepare_urls(pic_matches)
-
-# url = input("Enter Instagram url : ")
-# response = get_response(url)
-
-# vid_matches = re.findall('"video_url":"([^"]+)"',response)
-# pic_matches = re.findall('"display_url":"([^"]+)"', response)
-
-# vid_urls = prepare_urls(vid_matches)
-# pic_urls = prepare_urls(pic_matches)
-
-# if vid_urls:
-#     print(f"Detected Videos : \n{vid_urls}\n")
-
-# if pic_urls:
-#     print(f"Detected pictures : \n{pic_urls}\n")
-
-# if not(vid_urls or pic_urls):
-#     print("No videos or images found")
-
-
 
 
 if __name__ == '__main__':
